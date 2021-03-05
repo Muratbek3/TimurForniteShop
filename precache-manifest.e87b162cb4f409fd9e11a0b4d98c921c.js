6b433585c8fcbea0511862371c4b0a732732b5fd
Muratbek3 <45931103+Muratbek3@users.noreply.github.com>
2021-03-05 20:47:40 +0500

    Add files via upload

---
+++ precache-manifest.e87b162cb4f409fd9e11a0b4d98c921c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cdf94b3ef248fdec90cc618b2d3ab13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b721c920357dd430b01",
+    "url": "/static/css/2.37c27f5c.chunk.css"
+  },
+  {
+    "revision": "8da1d075a4142fe6956c",
+    "url": "/static/css/main.2cd7ab5c.chunk.css"
+  },
+  {
+    "revision": "6b721c920357dd430b01",
+    "url": "/static/js/2.b33eb152.chunk.js"
+  },
+  {
+    "revision": "2560076635a0d32cac6963ae2d9c2d16",
+    "url": "/static/js/2.b33eb152.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8da1d075a4142fe6956c",
+    "url": "/static/js/main.87c72c99.chunk.js"
+  },
+  {
+    "revision": "1415a29217de83bde8af",
+    "url": "/static/js/runtime-main.a8d5c61f.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  }
+]);
